f1268caa8ecf3a10e7d2b6f2328e0f0541dbf9b7
kavinkumar <kavinkumar1999@users.noreply.github.com>
2020-03-14 18:36:35 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import './h1.js';
+
+function App() {
+  return (
+    <div>
+<h1>Kavinkumar. ,Aloha!!</h1>
+        <a href='./h1.js'>link
+        </a>
+    </div>
+  );
+}
+
+export default App;
